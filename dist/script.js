fa29dee81705c95509bea4fbb20a1bc390cac7ed
Dominator-king <muzzrayyan04@gmail.com>
2023-07-07 17:14:58 +0500

    first commit

---
+++ dist/script.js
@@ -0,0 +1,5 @@
+function navDisplay() {
+  document.getElementById("mob-menu").classList.toggle("flex");
+  document.getElementById("mob-menu").classList.toggle("hidden");
+  document.getElementById("mob-open").classList.toggle("btn-animation");
+}
